31a085aa2f5ad4fcb002e7396aa0fb48b0de126a
yim222 <yimprogramming@gmail.com>
2024-04-04 20:59:40 +0300

    ???

--- Basic Class Structure/Program.cs
+++ Basic Class Structure/Program.cs
@@ -3,10 +3,13 @@
 namespace Basic_Class_Structure
 {
     class Program
-    {
-        static void Main(string[] args)
+    { 
+        static void Main(string[] args)//the entry point of the app. - נקודת הכניסה.
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World!222");
+            Test1.DoSomething2();
         }
+
+        
     }
 }
---
+++ Basic Class Structure/Test1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basic_Class_Structure
+{
+    public class Test1/*שם המחלקה */
+    {
+        public /*accessor modifier - בקר שליטה */ //private/protected
+            static /*Set this variable as static - מגדיר את המשתנה הזה בתור סטטי - כלומר קשור למחלקה ולא לשכפולים */
+            int /* Type of the variable - הטיפוס של המשתנה */  
+
+            myInt /* Name of the variable - שם המשתנה */
+                        = 7; /*The value that I give it - הערך שניתן למשתנה */
+        public static int myInt2 = 10;
+
+
+        /**ראו למעלה בעברית */ 
+        public/*accessor*/ 
+        static/* set it as static */
+            void /* the method type */
+            DoSomething()
+        {
+
+        }
+
+        public static String DoSomething2()
+        {
+
+            Console.WriteLine("DoSomething2 sadasd");
+            return "";
+        }
+    }
+}
